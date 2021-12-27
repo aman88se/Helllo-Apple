59fbfbcebad91db8a60aef347bb836881f887324
Aman Semwal <amansemwal542@gmail.com>
2021-12-27 12:35:12 +0900

    Initial Commit

---
+++ Helllo Apple/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Helllo Apple
+//
+//  Created by Aman Semwal on 2021/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Apple!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Helllo Apple/Helllo_AppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Helllo_AppleApp.swift
+//  Helllo Apple
+//
+//  Created by Aman Semwal on 2021/12/27.
+//
+
+import SwiftUI
+
+@main
+struct Helllo_AppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
